ff822af41974208171900735efcd5b1769e9a723
Maximo Hernandez <maximojhernandezla@gmail.com>
2024-02-05 20:39:36 -0300

    contact responsive

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -34,9 +34,9 @@ const AppRouter = () => {
         <div className="aboutUsContainer" ref={aboutUsRef}>
           <AboutUs />
         </div>
-        {/* <div className="contactContainer" ref={contactRef}>
+        <div className="contactContainer" ref={contactRef}>
           <Contact />
-        </div> */}
+        </div>
         <Footer />
       </div>
     </div>
