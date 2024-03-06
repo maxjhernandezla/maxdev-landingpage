0675d1655b0bf49fd02ec1b51cc25d137f298332
Maximo Hernandez <maximojhernandezla@gmail.com>
2024-03-06 15:21:24 -0300

    redes sociales añadidas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,35 @@ import { getLanguageData } from '../../helpers/languageHelper';
 
 const Footer = () => {
   const data = getLanguageData();
+  const wppLink = `https://wa.me/${import.meta.env.VITE_WHATSAPP_NUMBER}`;
+  const igLink = 'https://www.instagram.com/maxdev.ar';
+  const linkedInLink = 'https://www.linkedin.com/company/maxdev/about/';
   return (
     <div className="footer">
       <p>{data.footer.copyright}</p>
+      <div className="social">
+        <a href={linkedInLink} target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://res.cloudinary.com/maxjhernandez/image/upload/v1709744003/Disen%CC%83o_sin_ti%CC%81tulo_37_u1tbzy.png"
+            alt="linkedIn"
+            className="Img"
+          />
+        </a>
+        <a href={igLink} target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://res.cloudinary.com/maxjhernandez/image/upload/v1709744003/Disen%CC%83o_sin_ti%CC%81tulo_35_qqklbi.png"
+            alt="instagram"
+            className="Img"
+          />
+        </a>
+        <a href={wppLink} target="_blank" rel="noopener noreferrer">
+          <img
+            src="https://res.cloudinary.com/maxjhernandez/image/upload/v1709744962/Disen%CC%83o_sin_ti%CC%81tulo_38_dnrijy.png"
+            alt="whatsapp"
+            className="Img"
+          />
+        </a>
+      </div>
     </div>
   );
 };
